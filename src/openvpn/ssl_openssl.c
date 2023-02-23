11f2079b0005640afd588957c3f0cf01df4a640c
김 무형 <emusal@Mac-mini.local>
2023-02-23 10:40:11 +0900

    'NID_oqs_kem_default' in 'openssl/obj_mac.h' is deprecated. Replace to 'NID_frodo640aes' which is the smallest value among KEM-relate NIDs defined in 'openssl/obj_mac.h'.

--- src/openvpn/ssl_openssl.c
+++ src/openvpn/ssl_openssl.c
@@ -674,7 +674,7 @@ tls_ctx_load_ecdh_params(struct tls_root_ctx *ctx, const char *curve_name
     /* Check if this is a post-quantum algorithm from OQS. This assumes KEMs will always have the highest NIDs, hence no upper bound;
      * this is true as of OQS-OpenSSL 1.1.1-rc2. If this ever changes, update this if check to reflect the correct range of NIDs for KEMs.
      * NIDs are located in OpenSSL's include/openssl/obj_mac.h. */
-    if (nid >= NID_oqs_kem_default)
+    if (nid >= NID_frodo640aes)
     {
         /* Set the specified "curve" for TLS */
         int nid_list[1] = {nid};
@@ -1854,7 +1854,7 @@ print_details(struct key_state_ssl *ks_ssl, const char *prefix)
                          SSL_CIPHER_get_version(ciph),
                          SSL_CIPHER_get_name(ciph),
                          group_id,
-                         (group_id >= NID_oqs_kem_default)?"":"NOT ");
+                         (group_id >= NID_frodo640aes)?"":"NOT ");
     }
     else
     {
